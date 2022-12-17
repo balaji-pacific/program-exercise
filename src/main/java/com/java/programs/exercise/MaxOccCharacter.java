6e2fcd6d21f236532baaec94781b263bce6e62dc
Balaji Soundarrajan <balaji.pacific@gmail.com>
2022-12-17 16:16:55 -0500

    Adding another logic

--- src/main/java/com/java/programs/exercise/MaxOccCharacter.java
+++ src/main/java/com/java/programs/exercise/MaxOccCharacter.java
@@ -1,6 +1,7 @@
 package com.java.programs.exercise;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -40,8 +41,36 @@ public class MaxOccCharacter {
 			}
 		}
 		
-		System.out.println("Maximum Occurence is " + max.getKey() + " occured " + max.getValue());
-		System.out.println("Minimum Occurence is " + min.getKey() + " occured " + min.getValue());
+		//System.out.println("Maximum Occurence is " + max.getKey() + " occured " + max.getValue());
+		//System.out.println("Minimum Occurence is " + min.getKey() + " occured " + min.getValue());
+		
+		findMaxOccChar();
+		
+	}
+	
+	public static void findMaxOccChar() {
+		
+		String str = "ASKDJGNETTKDSDWSAS";
+		HashMap<String, Integer> actualMap = new HashMap<String, Integer>();
+		LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+		for(int i=0;i<str.length();i++) {
+			 Integer value = actualMap.get(String.valueOf(str.charAt(i)));
+			 if(value == null) {
+				 actualMap.put(String.valueOf(str.charAt(i)), 1);
+			 }else {
+				 actualMap.put(String.valueOf(str.charAt(i)), value + 1);
+			 }
+		}
+		actualMap.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
+		int count =1;
+		for(Map.Entry<String, Integer> me : sortedMap.entrySet()) {
+			if(count==1) {
+				System.out.println(me.getKey() + " Min Occured : " + me.getValue());
+			}else if(count == sortedMap.size()) {
+				System.out.println(me.getKey() + " Max Occured : " + me.getValue());
+			}
+			count++;
+		}
 		
 	}
 }
